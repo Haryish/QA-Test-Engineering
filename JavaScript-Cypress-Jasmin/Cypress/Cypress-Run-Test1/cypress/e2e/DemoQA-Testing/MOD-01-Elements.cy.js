3b148bd3285d5e7b6666d375779e7289c88b229f
Haryish E <haryishkumaran16@gmail.com>
2025-02-23 13:39:42 +0530

    Commit Cy.2: MOD-01-TS-001_Elements_Text Box Design and Execution completed

--- JavaScript-Cypress-Jasmin/Cypress/Cypress-Run-Test1/cypress/e2e/DemoQA-Testing/MOD-01-Elements.cy.js
+++ JavaScript-Cypress-Jasmin/Cypress/Cypress-Run-Test1/cypress/e2e/DemoQA-Testing/MOD-01-Elements.cy.js
@@ -1,39 +1,93 @@
 /// <reference types = "cypress" />
-
 import 'cypress-xpath';
 
-
 describe("TS-001-Text Box", () => {
+  
+  beforeEach(() => {
+    cy.visit("https://demoqa.com/");
+    cy.intercept('GET', '*adtrafficquality*', { statusCode: 403 }).as('blockAds');
 
-    beforeEach(() => {
-        cy.visit("https://demoqa.com/")
-        cy.intercept('GET', '*adtrafficquality*', { statusCode: 403 }).as('blockAds');
-        cy.xpath("//h5[contains(text(),'Elements')]").click()
-        cy.intercept('GET', '*adtrafficquality*', { statusCode: 403 }).as('blockAds');
+    cy.xpath("//h5[contains(text(),'Elements')]")
+      .should("be.visible")
+      .click();
+    cy.xpath("//span[contains(text(),'Text Box')]").click();
+    cy.url().should("include", "/text-box");
 
-    })
+  });
 
-    it("TC001_01-Validate text input submission", () => {
-        cy.intercept('GET', '*adtrafficquality*', { statusCode: 403 }).as('blockAds');
-        cy.url().should("include", "/elements")
-        cy.xpath("//span[contains(text(),'Text Box')]").click()
-        cy.get("#userName").type("Haryish")
-        cy.get("#userEmail").type("haryishkumaran16@gmail.com")
-        cy.get("#currentAddress").type("23,XYZ,\n sdsd street \n City-YYUJSD")
-        cy.xpath("//textarea[@id='permanentAddress']").type("23,XYZ,\n sdsd street \n City-YYUJSD")
-        cy.xpath("//button[@id='submit']").click()
+  it("TC001_01 - Validate text input submission", () => {
+    // cy.url().should("include", "/elements");
+    cy.fixture('testdata-TS01').then((data) => {
+      data.positiveCases.forEach((user) => {
+        cy.get("#userName").clear().type(user.fullName);
+        cy.get("#userEmail").clear().type(user.email);
+        cy.get("#currentAddress").clear().type(user.currentAddress);
+        cy.get("#permanentAddress").clear().type(user.permanentAddress);
+
+        cy.xpath("//button[@id='submit']").click();
         
-        // multiple web elemnt handling in cyoress
-        cy.xpath("//div[@id='output']//p").each(($el, index) => {
-            if (index === 0) {
-              expect($el.text()).to.include("Haryish");
-            } else if (index === 1) {
-              expect($el.text()).to.include("@gmail.com");
-            }
-          });
+        cy.xpath("//div[@id='output']//p").should('exist').each(($el, index) => {
+          if (index === 0) {
+            expect($el.text()).to.include(user.fullName);
+          } else if (index === 1) {
+            expect($el.text()).to.include(user.email);
+          } else if (index === 2) {
+            expect($el.text()).to.include(user.currentAddress);
+          } else if (index === 3) {
+            expect($el.text()).to.include(user.permanentAddress);
+          } 
+        });
+      });
+    });
+  });
 
+  it("TC001_02 - Verify error message for invalid email", () => {
+    // cy.url().should("include", "/elements");
+    cy.fixture('testdata-TS01').then((data) => {
+      data.negativeCases.forEach((user) => {
+        cy.get("#userName").clear().type(user.fullName || " ");
+        cy.get("#userEmail").clear().type(user.email || " ");
+        cy.get("#currentAddress").clear().type(user.currentAddress || " ");
+        cy.get("#permanentAddress").clear().type(user.permanentAddress || " ");
         
+        cy.xpath("//button[@id='submit']").click();
+        
+        // ✅ Form should NOT submit, so `#output` should NOT exist
+        cy.xpath("//div[@id='output']//p").should("not.exist");
 
-    })
+        // // ✅ Optional: Check for expected error messages (if applicable)
+        // cy.get("#userEmail").then(($el) => {
+        //   const borderColor = $el.css("border-color");
+        //   expect(borderColor).to.equal("rgb(255, 0, 0)"); // Red border for invalid email
+        // });     
+      });
+    });
+  });
 
-})
+  it("TC001_03 - Validate text input submission at edge level", () => {
+    // cy.url().should("include", "/elements");
+    cy.fixture('testdata-TS01').then((data) => {
+      data.edgeCases.forEach((user) => {
+        cy.get("#userName").type(user.fullName);
+        cy.get("#userEmail").type(user.email);
+        cy.get("#currentAddress").type(user.currentAddress);
+        cy.get("#permanentAddress").type(user.permanentAddress);
+
+        cy.xpath("//button[@id='submit']").click();
+        
+        cy.xpath("//div[@id='output']//p").should('exist').each(($el, index) => {
+          if (index === 0) {
+            expect($el.text()).to.include(user.fullName);
+          } else if (index === 1) {
+            expect($el.text()).to.include(user.email);
+          } else if (index === 2) {
+            expect($el.text()).to.include(user.currentAddress);
+          } else if (index === 3) {
+            expect($el.text()).to.include(user.permanentAddress);
+          } 
+        });
+      });
+    });
+  });
+
+});
